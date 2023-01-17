d202809f6cefb6ffe6a8d751d318833766bc7c5b
Indra Oki Sandy <indraokisandy1@gmail.com>
2023-01-17 12:41:51 +0700

    Modify Startscreen

--- lib/startscreen.dart
+++ lib/startscreen.dart
@@ -6,7 +6,26 @@ class StartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {},
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color(0xff006699),
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+          ),
+          child: Padding(
+            padding: EdgeInsets.all(
+              MediaQuery.of(context).size.width / 25,
+            ),
+            child: const Text(
+              'START',
+              style: TextStyle(fontSize: 35),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
